3599c061abffe6a817fcfe7c55125a3c99463be3
Ruediger Gad <r.c.g@gmx.de>
2015-09-27 22:04:16 +0200

    Further extend example.

--- src/cli4clj/example.clj
+++ src/cli4clj/example.clj
@@ -19,13 +19,15 @@
   (/ x y))
 
 (defn -main [& args]
+;;; Simple cli4clj usage example:
 ;;; In the simplest scenario only the commands have to be defined.
-;;; In the following a simple scenario with a few explanations is provided:
-;;;               Commands are stored in a nested map using the key :cmds in the options map.
-;;;               !      As simple example command, we define a command called "test".
-;;;               !      !      The function to be called for the command is defined via :fn.
-;;;               !      !      !  In this example we use an anonymous function.
-;;;               !      !      !  However, arbitrary functions can be used.
+;;; In the following, an example using a simple scenario along with some explanations is provided:
+;;;     cli4clj is configured via an "options" map.
+;;;     !         Commands are stored in a nested map using the key :cmds in the options map.
+;;;     !         !      As first simple example command, we define a command called "test".
+;;;     !         !      !      The function to be called for the command is defined via :fn.
+;;;     !         !      !      !  In this example we use an anonymous function.
+;;;     !         !      !      !  However, arbitrary functions can be used.
   (let [cli-opts {:cmds {:test {:fn #(println "This is a test.")
 ;;;                             Optionally a short information text can be given.
                                 :short-info "Test Command"
@@ -43,16 +45,22 @@
                                 :short-info "Add two values."}
                          :a :add
 ;;;                      The following example shows the use of a named function.
+;;;                      The divide function is also used to illustrate the behavior on errors during processing (e.g., try to divide by 0).
                          :divide {:fn divide
                                   :short-info "Divide two values."
                                   :long-info "The first argument will be divided by the second argument."}
                          :d :divide
-;;;                      "h" is already a pre-defined command but it can be overridden.
+;;;                      cli4clj already provides some pre-defined commands, from which some can overridden while others cannot.
+;;;                      "h" is a pre-defined command but it can be overridden.
 ;;;                      This is shown by using the definition of an alias to test as example.
                          :h :test
 ;;;                      The pre-defined "help" command, however, cannot be overridden.
 ;;;                      Please note that "help" and "exit" cannot be overridden by any user defined command.
                          :help :test
+;;;                      The following simple example command is used to illustrate that arbitrary Clojure data types can be used.
+;;;                      The command is intended to take a seq (e.g., a list or vector) which it converts into a CSV string.
+                         :to-csv {:fn (fn [data] (reduce (fn [s d] (str s "," d)) (str (first data)) (rest data)))
+                                  :short-info "Seq to CSV"}
                         }
                  }]
 ;;;  Last but not least, the CLI is started using the options defined above.
