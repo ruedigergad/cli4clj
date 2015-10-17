bf52cef45f37346ad997a9d5119c1d6ae0a13b6c
Ruediger Gad <r.c.g@gmx.de>
2015-10-17 09:49:45 +0200

    Add dependency to jline2.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-assorted-utils "1.9.1"]]
+                 [clj-assorted-utils "1.9.1"]
+                 [jline/jline "2.13"]]
   :main cli4clj.example)
