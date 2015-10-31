af1863819d76246d3cbeab944247a70aac5ebae1
Ruediger Gad <r.c.g@gmx.de>
2015-10-31 13:48:29 +0100

    Add example tests to demonstrate testing of a main method.

---
+++ test/cli4clj/test/example.clj
@@ -0,0 +1,44 @@
+;;;
+;;;   Copyright 2015 Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Example tests for demonstrating the testing of a main method."}
+  cli4clj.test.example
+  (:use clojure.test
+        cli4clj.cli
+        cli4clj.example
+        clj-assorted-utils.util))
+
+(deftest simple-main-method-example-test
+  (let [test-cmd-input ["t"]
+        out-string (test-cli-stdout #(-main "") test-cmd-input)]
+    (is (= "This is a test." out-string))))
+
+(deftest advanced-main-method-example-test
+  (let [test-cmd-input ["add 1 2"
+                        "divide 4 2"
+                        "d 3 2"]
+        out-string (test-cli-stdout #(-main "") test-cmd-input)]
+    (is (= (expected-string ["3" "2" "3/2"]) out-string))))
+
+(deftest simple-main-method-error--example-test
+  (let [test-cmd-input ["divide 4 0"]
+        out-string (test-cli-stderr #(-main "") test-cmd-input)]
+    (is (= "Divide by zero" out-string))))
+
+(deftest advanced-main-method-error-example-test
+  (let [test-cmd-input ["xyz"
+                        "d 4 0"
+                        "a 1 2 3"]
+        out-string (test-cli-stderr #(-main "") test-cmd-input)]
+    (is (= (expected-string ["Invalid command: \"[xyz]\". Please type \"help\" to get an overview of commands."
+                             "Divide by zero"
+                             "Wrong number of args (3) passed to: example/-main/fn--117"])
+           out-string))))
+
