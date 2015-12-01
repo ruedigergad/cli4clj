59604e8d876838c8deaf54e37c49a753e9856dad
Ruediger Gad <r.c.g@gmx.de>
2015-12-01 19:37:27 +0100

    Add eastwood as repl dependency.

--- project.clj
+++ project.clj
@@ -14,4 +14,6 @@
   :test2junit-output-dir "ghpages/test-results"
   :test2junit-run-ant true
   :main cli4clj.example
-  :plugins [[lein-cloverage "1.0.2"] [test2junit "1.1.3"] [lein-html5-docs "3.0.3"]])
+  :plugins [[lein-cloverage "1.0.2"] [test2junit "1.1.3"] [lein-html5-docs "3.0.3"]]
+  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.2.2" :exclusions  [org.clojure/clojure]]]}}
+)
