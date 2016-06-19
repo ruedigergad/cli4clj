7d1855ffe5bc8b41c26ae37640de5bcc89d9dbe8
Ruediger Gad <r.c.g@gmx.de>
2016-06-19 22:00:51 +0200

    Switch back to snapshot version.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cli4clj "1.1.1"
-;(defproject cli4clj "1.1.1-SNAPSHOT"
+;(defproject cli4clj "1.1.1"
+(defproject cli4clj "1.1.2-SNAPSHOT"
   :description "Create simple interactive CLIs for Clojure applications."
   :url "https://github.com/ruedigergad/cli4clj"
   :license {:name "Eclipse Public License"
