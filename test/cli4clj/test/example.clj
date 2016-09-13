4b87ac993e56b75bbcbbcd2adeeed5e3929c1167
Ruediger Gad <r.c.g@gmx.de>
2016-09-13 19:20:47 +0200

    Clean up example tests.

--- test/cli4clj/test/example.clj
+++ test/cli4clj/test/example.clj
@@ -12,74 +12,74 @@
   cli4clj.test.example
   (:require
     (clojure
-      [test :refer :all])
+      [test :as test])
     (cli4clj
-      [cli :refer :all]
-      [cli-tests :refer :all]
-      [example :refer :all])
-    (clj-assorted-utils [util :refer :all])))
+      [cli :as cli]
+      [cli-tests :as cli-tests]
+      [example :as example])
+    (clj-assorted-utils [util :as utils])))
 
-(deftest simple-main-method-example-test
+(test/deftest simple-main-method-example-test
   (let [test-cmd-input ["t"]
-        out-string (test-cli-stdout #(-main "") test-cmd-input)]
-    (is (= "This is a test." out-string))))
+        out-string (cli-tests/test-cli-stdout #(example/-main "") test-cmd-input)]
+    (test/is (= "This is a test." out-string))))
 
-(deftest advanced-main-method-example-test
+(test/deftest advanced-main-method-example-test
   (let [test-cmd-input ["add 1 2"
                         "divide 4 2"
                         "d 3 2"]
-        out-string (test-cli-stdout #(-main "") test-cmd-input)]
-    (is (= (expected-string ["3" "2" "3/2"]) out-string))))
+        out-string (cli-tests/test-cli-stdout #(example/-main "") test-cmd-input)]
+    (test/is (= (cli-tests/expected-string ["3" "2" "3/2"]) out-string))))
 
-(deftest simple-main-method-error-example-test
+(test/deftest simple-main-method-error-example-test
   (let [test-cmd-input ["divide 4 0"]
-        out-string (test-cli-stderr #(-main "") test-cmd-input)]
-    (is (= "Divide by zero" out-string))))
+        out-string (cli-tests/test-cli-stderr #(example/-main "") test-cmd-input)]
+    (test/is (= "Divide by zero" out-string))))
 
-(deftest advanced-main-method-error-example-test
+(test/deftest advanced-main-method-error-example-test
   (let [test-cmd-input ["xyz"
                         "d 4 0"]
-        out-string (test-cli-stderr #(-main "") test-cmd-input)]
-    (is (= (expected-string ["Invalid command: \"[xyz]\". Please type \"help\" to get an overview of commands."
+        out-string (cli-tests/test-cli-stderr #(example/-main "") test-cmd-input)]
+    (test/is (= (cli-tests/expected-string ["Invalid command: \"[xyz]\". Please type \"help\" to get an overview of commands."
                              "Divide by zero"])
            out-string))))
 
-(deftest main-method-error-with-trace-example-test
+(test/deftest main-method-error-with-trace-example-test
   (let [test-cmd-input ["_enable-trace true"
                         "divide 4 0"]
-        out-string (test-cli-stdout #(-main "") test-cmd-input)]
-    (is (.startsWith
+        out-string (cli-tests/test-cli-stdout #(example/-main "") test-cmd-input)]
+    (test/is (.startsWith
           out-string
-          (expected-string ["print-exception-trace is set to: true"
+          (cli-tests/expected-string ["print-exception-trace is set to: true"
                             "java.lang.ArithmeticException: Divide by zero"
                             " at clojure.lang.Numbers.divide (Numbers.java"])))))
 
-(deftest print-function-test
+(test/deftest print-function-test
   (let [test-cmd-input ["print 1701"]
-        out-string (test-cli-stdout #(-main "") test-cmd-input)]
-    (is
+        out-string (cli-tests/test-cli-stdout #(example/-main "") test-cmd-input)]
+    (test/is
       (=
-        (expected-string
+        (cli-tests/expected-string
           ["Arg-type: java.lang.Long Arg: 1701"
            "Opt-args: nil"])
         out-string))))
 
-(deftest invalid-token-to-string-fallback-test
+(test/deftest invalid-token-to-string-fallback-test
   (let [test-cmd-input ["print /foo/bar"]
-        out-string (test-cli-stdout #(-main "") test-cmd-input)]
-    (is
+        out-string (cli-tests/test-cli-stdout #(example/-main "") test-cmd-input)]
+    (test/is
       (=
-        (expected-string
+        (cli-tests/expected-string
           ["Arg-type: java.lang.String Arg: \"/foo/bar\""
            "Opt-args: nil"])
         out-string))))
 
-(deftest print-function-opt-args-test
+(test/deftest print-function-opt-args-test
   (let [test-cmd-input ["print foo bar baz"]
-        out-string (test-cli-stdout #(-main "") test-cmd-input)]
-    (is
+        out-string (cli-tests/test-cli-stdout #(example/-main "") test-cmd-input)]
+    (test/is
       (=
-        (expected-string
+        (cli-tests/expected-string
           ["Arg-type: clojure.lang.Symbol Arg: foo"
            "Opt-args: (bar baz)"])
         out-string))))
