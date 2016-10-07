84235f4084a555e3983a4fb5eb31bc513b9158b4
Ruediger Gad <r.c.g@gmx.de>
2016-10-07 07:51:57 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-assorted-utils "1.12.0"]
+                 [clj-assorted-utils "1.13.0"]
                  [jline/jline "2.14.2"]]
   :global-vars {*warn-on-reflection* true}
   :html5-docs-docs-dir "ghpages/doc"
@@ -14,6 +14,6 @@
   :test2junit-output-dir "ghpages/test-results"
   :test2junit-run-ant true
   :main cli4clj.example
-  :plugins [[lein-cloverage "1.0.2"] [test2junit "1.1.3"] [lein-html5-docs "3.0.3"]]
+  :plugins [[lein-cloverage "1.0.2"] [test2junit "1.2.3"] [lein-html5-docs "3.0.3"]]
   :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.2.2" :exclusions  [org.clojure/clojure]]]}}
 )
