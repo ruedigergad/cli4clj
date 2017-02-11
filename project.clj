0f516023e8276c60e915e899a4cf6cdee80cbdd0
Ruediger Gad <r.c.g@gmx.de>
2017-02-11 22:39:17 +0100

    Update dependencies and version info and deploy to clojars.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-;(defproject cli4clj "1.2.5"
-(defproject cli4clj "1.2.6-SNAPSHOT"
+(defproject cli4clj "1.2.6"
+;(defproject cli4clj "1.2.6-SNAPSHOT"
   :description "Create simple interactive CLIs for Clojure applications."
   :url "https://github.com/ruedigergad/cli4clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-assorted-utils "1.15.0"]
+                 [clj-assorted-utils "1.17.1"]
                  [jline/jline "2.14.2"]]
   :global-vars {*warn-on-reflection* true}
   :html5-docs-docs-dir "ghpages/doc"
