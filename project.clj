6828b1b84df09e9e265704b98f61ce2f18d080d6
Ruediger Gad <r.c.g@gmx.de>
2018-09-11 17:09:29 +0200

    Update version info and deploy to clojars.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cli4clj "1.6.1"
-;(defproject cli4clj "1.6.2-SNAPSHOT"
+(defproject cli4clj "1.6.2"
+;(defproject cli4clj "1.6.3-SNAPSHOT"
   :description "Create simple interactive CLIs for Clojure applications."
   :url "https://github.com/ruedigergad/cli4clj"
   :license {:name "Eclipse Public License"
