f6c670fd93fbaed2fe7dae28f2be807ace8ae9c1
Ruediger Gad <r.c.g@gmx.de>
2021-04-02 19:42:44 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 ; Revert to Clojure 1.9.0 until the following is fixed:
 ; https://dev.clojure.org/jira/browse/CLJ-1472
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [clj-assorted-utils "1.18.5"]
-                 [org.clojure/core.async "1.2.603"]
+                 [org.clojure/core.async "1.3.610"]
                  [jline/jline "2.14.6"]]
   :global-vars {*warn-on-reflection* true}
   :html5-docs-docs-dir "ghpages/doc"
@@ -19,5 +19,5 @@
   :main cli4clj.example
   :aot :all
   :plugins [[lein-cloverage "1.0.2"] [test2junit "1.3.3"] [lein-html5-docs "3.0.3"]]
-  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.3.11" :exclusions  [org.clojure/clojure]]]}}
+  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.4.0" :exclusions  [org.clojure/clojure]]]}}
 )
