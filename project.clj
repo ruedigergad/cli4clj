db5e6683c3e8874a6e94f50bc846a22258aff09e
Ruediger Gad <r.c.g@gmx.de>
2021-04-11 11:54:21 +0200

    Update version info and dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;(defproject cli4clj "1.7.8"
-(defproject cli4clj "1.7.9-SNAPSHOT"
+(defproject cli4clj "1.7.9"
+;(defproject cli4clj "1.7.19-SNAPSHOT"
   :description "Create simple interactive CLIs for Clojure applications."
   :url "https://github.com/ruedigergad/cli4clj"
   :license {:name "Eclipse Public License"
@@ -7,17 +7,17 @@
 ; Revert to Clojure 1.9.0 until the following is fixed:
 ; https://dev.clojure.org/jira/browse/CLJ-1472
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [clj-assorted-utils "1.18.7"]
+                 [clj-assorted-utils "1.18.8"]
                  [org.clojure/core.async "1.3.610"]
                  [jline/jline "2.14.6"]]
   :global-vars {*warn-on-reflection* true}
-  :html5-docs-docs-dir "ghpages/doc"
+  :html5-docs-docs-dir "docs/doc"
   :html5-docs-ns-includes #"^cli4clj.*"
   :html5-docs-repository-url "https://github.com/ruedigergad/cli4clj/blob/master"
-  :test2junit-output-dir "ghpages/test-results"
+  :test2junit-output-dir "docs/test-results"
   :test2junit-run-ant true
   :main cli4clj.example
   :aot :all
-  :plugins [[lein-cloverage "1.0.2"] [test2junit "1.3.3"] [lein-html5-docs "3.0.3"]]
+  :plugins [[lein-cloverage "1.0.9"] [test2junit "1.4.2"] [lein-html5-docs "3.0.3"]]
   :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.4.0" :exclusions  [org.clojure/clojure]]]}}
 )
