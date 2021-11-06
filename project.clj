c1b4f42f1d5e578305c8a251cf1c640560cab222
Ruediger Gad <r.c.g@gmx.de>
2021-11-06 20:33:43 +0100

    Update dependencies and version info.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;(defproject cli4clj "1.7.9"
-(defproject cli4clj "1.7.19-SNAPSHOT"
+(defproject cli4clj "1.7.10"
+;(defproject cli4clj "1.7.11-SNAPSHOT"
   :description "Create simple interactive CLIs for Clojure applications."
   :url "https://github.com/ruedigergad/cli4clj"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
 ; https://dev.clojure.org/jira/browse/CLJ-1472
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [clj-assorted-utils "1.18.8"]
-                 [org.clojure/core.async "1.3.610"]
+                 [org.clojure/core.async "1.4.627"]
                  [jline/jline "2.14.6"]]
   :global-vars {*warn-on-reflection* true}
   :html5-docs-docs-dir "docs/doc"
@@ -19,5 +19,5 @@
   :main cli4clj.example
   :aot :all
   :plugins [[lein-cloverage "1.0.9"] [test2junit "1.4.2"] [lein-html5-docs "3.0.3"]]
-  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.4.0" :exclusions  [org.clojure/clojure]]]}}
+  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.9.9" :exclusions  [org.clojure/clojure]]]}}
 )
